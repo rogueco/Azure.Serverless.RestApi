9ff3de868b347547661b9423f3c0573a5b50f689
Fletchy <tom@tomfletcher.tech>
2021-05-30 20:29:37 +0100

    Deployed to Azure.

---
+++ ServerlessRestApi.TodoFunction/Functions/QueueListeners.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using ServerlessRestApi.TodoFunction.Entities;
+
+namespace ServerlessRestApi.TodoFunction.Functions
+{
+    public static class QueueListeners
+    {
+        [FunctionName("QueueListeners")]
+        public static async Task RunAsync([QueueTrigger("todos", Connection = "AzureWebJobsStorage")]
+            Todo todo,
+            [Blob("todos", Connection = "AzureWebJobsStorage")]
+            CloudBlobContainer container,
+            ILogger log)
+        {
+            await container.CreateIfNotExistsAsync();
+            CloudBlockBlob blob = container.GetBlockBlobReference($"{todo.Id}.txt");
+            await blob.UploadTextAsync($"Create a new task: {todo.TaskDescription}");
+            log.LogInformation($"C# Queue trigger function processed: {todo.TaskDescription}");
+        }
+    }
+}
---
+++ ServerlessRestApi.TodoFunction/Functions/ScheduledFunction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using ServerlessRestApi.TodoFunction.Entities;
+using  Microsoft.Azure.Cosmos.Table;
+
+namespace ServerlessRestApi.TodoFunction.Functions
+{
+    public static class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static async Task RunAsync(
+            [TimerTrigger("0 */5 * * * *")] TimerInfo myTimer,
+            [Table("todos", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            TableQuery<TodoTableEntity> query = new TableQuery<TodoTableEntity>();
+            TableQuerySegment<TodoTableEntity> segment = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+            int deleted = 0;
+            foreach (TodoTableEntity todoTableEntity in segment)
+            {
+                if (todoTableEntity.IsCompleted)
+                {
+                    await todoTable.ExecuteAsync(TableOperation.Delete(todoTableEntity));
+                    deleted++;
+                }
+            }
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+            log.LogInformation($"Deleted {deleted} item at {DateTime.UtcNow}");
+        }
+    }
+}
